37db5e7243a0c7bed6a46228a0a2cc322cc33ec9
serkutYILDIRIM <serkutyldrm@gmail.com>
2025-06-14 23:06:58 +0300

    unnecessary usings removed

--- GoldenLibrary/Data/Concrete/EfCore/EfPostRepository.cs
+++ GoldenLibrary/Data/Concrete/EfCore/EfPostRepository.cs
@@ -1,9 +1,6 @@
 ﻿using GoldenLibrary.Data.Abstract;
 using GoldenLibrary.Entity;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace GoldenLibrary.Data.Concrete.EfCore
 {
