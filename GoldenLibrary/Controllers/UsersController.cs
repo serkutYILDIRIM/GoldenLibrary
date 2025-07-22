e919b0cc8ad4cfc9f38249efa034a40498601b3f
serkutYILDIRIM <serkutyldrm@gmail.com>
2025-07-22 23:08:36 +0300

    some refactor

--- GoldenLibrary/Controllers/UsersController.cs
+++ GoldenLibrary/Controllers/UsersController.cs
@@ -107,9 +107,8 @@ namespace GoldenLibrary.Controllers
         public IActionResult Profile(string username)
         {
             if (string.IsNullOrEmpty(username))
-            {
                 return NotFound();
-            }
+
             var user = _userRepository
                         .Users
                         .Include(x => x.Posts)
